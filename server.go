337c81a0dd77926a37989fc09e33b8c35629e2ac
hoashi <akane.hoashi@outlook.jp>
2020-10-06 16:26:24 +0900

    追加: モジュール, db接続処理

--- server.go
+++ server.go
@@ -1,25 +1,49 @@
 package main
 
 import (
+	"github.com/jinzhu/gorm"
 	"log"
 	"net/http"
 	"os"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/hoashi-akane/moneygement-graphql/graph"
 	"github.com/hoashi-akane/moneygement-graphql/graph/generated"
 )
 
-const defaultPort = "8080"
 
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
+	savingDb, err := gorm.Open("mysql", dataSourceSavings)
+	usrDb, err := gorm.Open("mysql", dataSourceUser)
 
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	if err != nil{
+		panic(err)
+	}
+	if savingDb == nil || usrDb == nil{
+		panic(err)
+	}
+	defer func(){
+		if savingDb != nil{
+			if err := savingDb.Close(); err != nil{
+				panic(err)
+			}
+		}
+		if usrDb != nil{
+			if err := usrDb.Close(); err != nil{
+				panic(err)
+			}
+		}
+	}()
+	savingDb.LogMode(true)
+	usrDb.LogMode(true)
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{SAVDB: savingDb, USRDB: usrDb}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
