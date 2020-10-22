4613c56972fc07d163d170f2be9c64cbdf509717
hoashi <akane.hoashi@outlook.jp>
2020-10-22 12:42:50 +0900

    変更：URL変更

--- server.go
+++ server.go
@@ -46,7 +46,7 @@ func main() {
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{SAVDB: savingDb, USRDB: usrDb}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	http.Handle("/graphql", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
